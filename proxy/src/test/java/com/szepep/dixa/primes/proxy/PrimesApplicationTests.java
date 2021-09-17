bf2e94309f8e104b63122ca3b2d32b47592cfe84
Peter Szepe <szepep@gmail.com>
2021-09-17 20:28:14 +0200

    Setting up the project and define protobuf messages and grpc service

---
+++ proxy/src/test/java/com/szepep/dixa/primes/proxy/PrimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szepep.dixa.primes.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ service/src/test/java/com/szepep/dixa/primes/service/PrimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szepep.dixa.primes.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
